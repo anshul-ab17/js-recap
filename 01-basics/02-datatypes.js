ad8c156b8758f7341e3b289352fc7a8f2ce56591
Anshul Bharat <96761799+anshul-ab17@users.noreply.github.com>
2024-12-09 23:57:29 +0000

    data

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,26 @@
+" use strict" // treat all the js code as new version.
+
+// alert(3+1) - not used here as it is used in the browser , here code execute in node.js
+
+// ecmascript- used to define the standard script and sturcture of the code.
+
+let str1 = "datatypes";
+console.log(str1)
+
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value e.g  NAN 
+undefined => // not declared.
+symbol => unique 
+
+
+// object 
+*/
+
+console.log(typeof str1)
+console.log(typeof null) // object as we can see in the output
+console.log(typeof undefined)
