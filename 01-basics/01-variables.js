b580b15997992b7f71c564d04b0e62bdb35244fe
Anshul Bharat <96761799+anshul-ab17@users.noreply.github.com>
2024-12-09 01:15:03 +0000

    variables

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,20 @@
+const student_Id = 2024
+
+let mail = "anshul.ab17x@gmail.com"
+
+var pswd = "23432"
+
+city = "Chandigarh"
+
+
+let address;
+console.log(city);
+
+// {} - scope
+//var not used much becoz of the issue in block scope and functional scope.
+
+
+console.table([mail, pswd, city, student_Id, address])
+
+
+
