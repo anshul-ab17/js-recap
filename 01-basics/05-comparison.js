0bf796a1421a114b00bc87dcc62c1399b6464aad
Anshul Bharat <96761799+anshul-ab17@users.noreply.github.com>
2024-12-12 11:58:18 +0000

    compare

---
+++ 01-basics/05-comparison.js
@@ -0,0 +1,15 @@
+
+
+console.log("2">1)
+ /* this kind of comparison doesn't give us correct result somemtimes
+ as both are differnt data types.
+
+ So, this is cured in the Typescript - which dooesn't allow us 
+ to comapre betwwen two data types.
+*/
+
+console.log([null >0,null ==0 ,null >=0])
+console.log([undefined >0,undefined ==0 ,undefined >=0])
+
+console.log("2"===2);
+console.log("2"==2);
