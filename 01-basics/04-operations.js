5464d717119b3f8c424dec0ec4b4eefd190867af
Anshul Bharat <96761799+anshul-ab17@users.noreply.github.com>
2024-12-12 11:23:59 +0000

    operations

--- 01-basics/04-operations.js
+++ 01-basics/04-operations.js
@@ -5,3 +5,24 @@ let neg = -value;
 console.log(neg);
 
 console.table([2+3,2-3,2*3,2/3,2%3, 2**3])
+
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+
+console.log([+true,-false,+"",-"", +"s",-"s"])
+
+
+let x = 3;
+const y = x++;
+
+console.log([x,y]);
+
+let a = 3;
+const b = ++a;
+
+console.log([a,b]);
