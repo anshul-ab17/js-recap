08ca051bd5a4277f1213e3c3940bb7428fdc9101
Anshul Bharat <96761799+anshul-ab17@users.noreply.github.com>
2024-12-24 14:27:29 +0000

    arrayyy

--- 01-basics/hello.js
+++ 01-basics/hello.js
@@ -2,3 +2,5 @@ console.log("ab")
 
 // in console there's a keyword called "touch" which is used to create a files
 // of any extension.
+
+// Prototypes are the mechanism by which JavaScript objects inherit features from one another
