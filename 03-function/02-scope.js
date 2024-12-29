e9a958635fce1fc8fce038f29668e36b74adfd13
Anshul Bharat <96761799+anshul-ab17@users.noreply.github.com>
2024-12-29 09:14:03 +0000

    hoisting

--- 03-function/02-scope.js
+++ 03-function/02-scope.js
@@ -31,45 +31,55 @@ console.log(c);
 
 // Block Scope: Variables accessible only within the block {} they are declared in, using let or const.
 
-// function one(){
-//     const username = "hitesh"
-
-//     function two(){
-//         const website = "youtube"
-//         console.log(username);
-//     }
-//     // console.log(website);
-
-//      two()
-
-// }
-
-// // one()
-
-// if (true) {
-//     const username = "hitesh"
-//     if (username === "hitesh") {
-//         const website = " youtube"
-//         // console.log(username + website);
-//     }
-//     // console.log(website);
-// }
-
-// // console.log(username);
+console.log("*****************************")
 
 
-// // ++++++++++++++++++ interesting ++++++++++++++++++
+function one(){
+    const username = "bharat"
+
+    function two(){
+        const website = "github"
+        console.log(username);
+    }
+    // console.log(website); 
+    //  two() 
+    // both code won't be executed
 
 
-// console.log(addone(5))
+}
+one()
 
-// function addone(num){
-//     return num + 1
-// }
+if (true) {
+    const username = "bharat"
+    if (username === "bharat") {
+        const website = " github"
+        console.log(username + website);
+    }
+    // console.log(website); not executed , out of scope
+}
 
 
 
-// addTwo(5)
-// const addTwo = function(num){
-//     return num + 2
-// }
+
+
+console.log(addone(5)) // executed before initilization
+function addone(n){
+    return n + 1
+}
+
+
+
+
+
+// addTwo(5) won't axecuted before initilization
+const addTwo = function(num){
+    return num + 2
+}
+
+console.log(addTwo(5))
+
+
+// Hoisting in JavaScript is a built-in behavior that moves the declarations of functions, variables, and classes to the top of their scope before the code is executed
+
+// Value hoisting: Using a variable's value in its scope before the line it is declared
+// Declaration hoisting: Referencing a variable in its scope before the line it is declared, but the value is always undefined
