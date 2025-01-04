115d19572c08083fbc610e437cfadbf030c92f21
Anshul Bharat <96761799+anshul-ab17@users.noreply.github.com>
2025-01-04 17:46:14 +0000

    loops

--- 05-iterations/02-while.js
+++ 05-iterations/02-while.js
@@ -1,46 +1,21 @@
-//----for of ------
 
-// for (const element of array1) {
-//     console.log(element);
-//   }
-
-const arr = [1, 2, 3, 4, 5]
-
-for (const num of arr) {
-    // console.log(num);
-}
-
-const greetings = "Hello world!"
-for (const greet of greetings) {
-    // console.log(`Each char is ${greet}`)
-}
-
-//-----Maps------
-//  Map object holds key-value pairs and remembers the original insertion order of the keys
-
-const map = new Map()
-map.set('IN', "India")
-map.set('USA', "United States of America")
-map.set('Fr', "France")
-map.set('IN', "India")// set unique value so this won't be added into the map
-
-
-console.log(map);
-
-for (const [key, value] of map) {
-    console.log(key, ':-', value);
-}
-
-
-// for object
-
-const myObject = {
-    'game1': 'NFS',
-    'game2': 'Spiderman'
-}
-
-//object is not iteratable like array
-// for (const [key, value] of myObject) {
-//     console.log(key);
-    
+let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
 // }
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    //console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
