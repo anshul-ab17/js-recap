5a74542d7fb7f2725197d50601bf1c5541d81db6
Anshul Bharat <96761799+anshul-ab17@users.noreply.github.com>
2025-01-06 04:56:48 +0000

    commit msg

--- 05-iterations/07-more.js
+++ 05-iterations/07-more.js
@@ -7,7 +7,7 @@ const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const newNums = myNumers
                 .map((num) => num * 10 )
-                .map( (num) => num + 1)
+                .map( (num) => num + 1) //-- here value of the num changes and taken from above.map func.
                 .filter( (num) => num >= 40)
 
 console.log(newNums);
